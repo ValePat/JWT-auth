6744a0bd82ad67aa424d3c30edb95149725fc65f
Valerio Patricolo <valerio.patricolo@innovatesapp.com>
2024-06-25 22:46:52 +0200

    connection to mySQL database + users operation endpoints and queries

--- server.js
+++ server.js
@@ -2,79 +2,98 @@ require('dotenv').config();
 const express = require("express");
 const jwt = require ('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const db = require('./db/database');
 
 const app = express();
 app.use(express.json());
-
-const users = [];
-const mockData = [
-    { userName: 'User1', data: 'unique auth data 1' },
-    { userName: 'User2', data: 'unique auth data 2' }
-];
-
 
 app.get("/", (req, res) => {
     res.status(200).send(users)
 });
 
-//Get data for authenticaded users only
-app.get("/getMockData", authenticateToken, (req, res) => {
-    res.json(mockData.filter(el => el.userName === req.user.name));
+app.get("/getData", authenticateToken, async (req, res) => {
+    
+    try{
+        const data = await db.query('SELECT * FROM Contacts');
+    }catch (e) {
+        res.status(500).send(e)
+    }
+    res.status(201).send(data);
 });
 
-//Register a new user to the database
 app.post("/users/register", async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = { name: req.body.name, password: hashedPassword };
-        users.push(user);
-        res.status(201).send(users);
-    } catch {
-        res.status(500).send();
+        const hashedPassword = await bcrypt.hash(req.body.PASSWORD, 10);
+        const user = { USER_NAME: req.body.USER_NAME, PASSWORD: hashedPassword };
+        const sInsert = 'INSERT INTO USERS (USER_NAME, PASSWORD) VALUES (?, ?)';
+        const result = await db.execute(sInsert, [user.USER_NAME, user.PASSWORD]);
+        res.status(201).send(result);
+    } catch (e) {
+        res.status(500).send(e);
     }
 });
 
-//Login and generate jwt token for authentication
 app.post("/users/login", async (req, res) => {
-    const user = users.find((user) => user.name === req.body.name);
+    
+    const { USER_NAME, PASSWORD } = req.body;
+    const sSelect = 'SELECT * FROM USERS WHERE USER_NAME = ?';
+    const rows = await db.execute(sSelect, [USER_NAME]);
+    const dbUser = rows[0];
 
-    if (!user) {
+    if (!dbUser) {
         return res.status(400).send("Cannot find user");
     }
 
     try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            const username = req.body.username
-            const user = { name: username }
-          
-            const accessToken = generateAccessToken(user)
-            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-            refreshTokens.push(refreshToken)
+        if (await bcrypt.compare(req.body.PASSWORD, dbUser[0].PASSWORD)) {
+            const username = req.body.USER_NAME
+            const jwtUser = { name: username }
+            const accessToken = generateAccessToken(jwtUser)
+            const refreshToken = jwt.sign(jwtUser, process.env.REFRESH_TOKEN_SECRET)
+            const sInsert = 'INSERT INTO AUTH (REFRESH_TOKEN) VALUES (?)';
+            await db.query(sInsert, [refreshToken]);
             res.json({ accessToken: accessToken, refreshToken: refreshToken })
         } else {
-            res.send("Not Allowed");
+            res.send("Password non corretta");
         }
-    } catch {
-        res.status(500).send();
+    } catch(e) {
+        res.status(500).send(e);
     }
 });
 
-let refreshTokens = []
+app.post('users/refresh', async(req, res) => {
 
-app.post('/token', (req, res) => {
   const refreshToken = req.body.token
   if (refreshToken == null) return res.sendStatus(401)
-  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    const accessToken = generateAccessToken({ name: user.name })
-    res.json({ accessToken: accessToken })
-  })
+
+    try{
+        const sSelect = 'SELECT * FROM AUTH WHERE REFRESH_TOKEN = ?';
+        const rows = await db.query(sSelect, [refreshToken]);
+        if (rows.length === 0) {
+            return res.sendStatus(403);
+        }
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, jwtUser) => {
+          if (err) return res.sendStatus(403)
+          const accessToken = generateAccessToken({ name: jwtUser.name })
+          res.json({ accessToken: accessToken })
+        })
+    }catch(e){
+
+    }
+    
 })
 
-app.delete('/logout', (req, res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-  res.sendStatus(204)
+app.delete('/users/logout', async (req, res) => {
+    const tokenToDelete = req.body.token;
+    const sDelete = 'DELETE FROM AUTH WHERE REFRESH_TOKEN = ?';
+
+    try{
+        const test = await db.execute(sDelete, [tokenToDelete])
+        console.log('Token di refresh eliminato con successo dal database');
+        res.status(204).send(test); 
+    }catch(e){
+        res.status(500).send(e)
+    }
 })
 
 
@@ -91,8 +110,8 @@ function authenticateToken(req, res, next) {
     });
 };
 
-function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+function generateAccessToken(jwtUser) {
+    return jwt.sign(jwtUser, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '59s' })
 };
 
 app.listen(3000, () => {
